7f4871677f0b9284256c2a511da806796c0ab48c
Shiori Miyazaki <2111140084u@ed.fuk.kindai.ac.jp>
2023-10-18 07:08:31 +0000

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,15 @@
+class TopControler < ApplicationController 
+    def main
+        if session[:login_uid] != nil
+            render "main"
+        else
+            render "login"
+        end
+    end
+    def login
+        if params[:uid] == 'kindai' and params[:pass] == 'sanriko'
+            session[:log_uid] = params[:uid]
+            redirect_to top_main_path
+        end
+    end
+end
